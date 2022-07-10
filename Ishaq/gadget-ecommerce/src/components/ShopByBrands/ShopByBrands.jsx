21a1081b5363f4e745d4494d365193024d79b765
Md Ishaque <iamishaque.business@gmail.com>
2022-07-10 07:34:21 +0600

    update read me file

---
+++ Ishaq/gadget-ecommerce/src/components/ShopByBrands/ShopByBrands.jsx
@@ -0,0 +1,21 @@
+
+export default function ShopByBrands() {
+   
+
+    return (
+        <>
+            <div className="relative container py-16 bg-[#F4F4F6] flex justify-center flex-col items-center">
+                <div className="flex flex-col items-center justify-center">
+                    <h3 className='font-extrabold md:text-3xl text-xl  text-dark bg-hero-vector flex justify-center items-center bg-contain bg-left bg-no-repeat  md:py-1 md:w-96 w-52 md:h-16 h-8 text-center'>
+                        Shop By Brands
+                    </h3>
+                </div>
+                {/* grid grid-cols-2 md:grid-cols-5 gap-3 */}
+                <div className="mt-16 relative w-full">
+                  
+
+                </div>
+            </div>
+        </>
+    )
+}
