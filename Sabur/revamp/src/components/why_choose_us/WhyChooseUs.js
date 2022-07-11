2aad3f5b2f36efbcb2bca759924e2bf7bfe70a3f
Abdus Sabur <49846108+saburali@users.noreply.github.com>
2022-07-11 10:30:23 +0600

    Why Choose use - section few updated

--- Sabur/revamp/src/components/why_choose_us/WhyChooseUs.js
+++ Sabur/revamp/src/components/why_choose_us/WhyChooseUs.js
@@ -4,14 +4,24 @@ import {Link} from "react-router-dom";
 
 import chat_img from '../../assets/images/chat.png';
 
+import Background from '../../assets/images/why_choose_us-bg.svg';
+
+
+
 class WhyChooseUs extends Component {
     render() {
+        const bgStyle = {
+            backgroundImage: "url(" + Background + ")",
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'left'
+        };
         return (
             <Fragment>
                 <div className="pt-5 pb-5 lightDeepBG-2">
                     <div className="container">
                         <div className="row">
-                            <div className="col-md-6 d-flex justify-content-center align-items-center d-none d-md-block">
+                            <div className="col-md-6 d-flex justify-content-center align-items-center d-none d-md-block" style={bgStyle}>
                                 <img src={chat_img} alt="image" className="img-fluid"/>
                             </div>
                             <div className="col-md-6 my-0 my-md-4 pt-4 pt-md-0 position-relative">
@@ -23,10 +33,15 @@ class WhyChooseUs extends Component {
                                         mollis suscipit diam. Phasellus interdum pharetra mi, et ultrices libero tincidunt vitae. Fusce volutpat nisi vitae arcu accumsan tempus.
                                         Quisque commodo commodo tellus, sed commodo nisl maximus in. Integer mattis finibus lorem sit amet efficitur. Sed id viverra eros,
                                         id malesuada elit. Proin vulputate eu lorem ac lacinia.  </p>
+                                    <p className="lightColor lh-160 pb-40 mb-12 pt-3 pt-md-0"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et lectus ut magna aliquet pharetra. Sed ipsum velit, mollis fermentum metus consequat,
+                                        mollis suscipit diam. Phasellus interdum pharetra mi, et ultrices libero tincidunt vitae. Fusce volutpat nisi vitae arcu accumsan tempus.
+                                        Quisque commodo commodo tellus, sed commodo nisl maximus in. Integer mattis finibus lorem sit amet efficitur. Sed id viverra eros,
+                                        id malesuada elit. Proin vulputate eu lorem ac lacinia.  </p>
+
                                 </div>
                                 <div className="position-absolute position-bottom-0 lightDeepBG-2 pt-3 pt-md-0 w-100">
                                     <Link to="/join" className="btn rounded-pill text-white themeBG btn-outline-success border-0 px-4 py-2">Join Today</Link>
-                                    <Link to="/join" className="btn rounded-pill darkColor btn-outline-success border border-success px-4 py-2 ms-3">Learn More</Link>
+                                    <Link to="/" className="btn rounded-pill darkColor btn-outline-success border border-success px-4 py-2 ms-3">Learn More</Link>
                                 </div>
                             </div>
                         </div>
