924dbf9bf15307b3d8985e11002e0b8c3ff00969
Abdus Sabur <49846108+saburali@users.noreply.github.com>
2022-07-21 18:23:54 +0600

    Contact Page - Done

--- Sabur/revamp/src/page/contact/Contact.js
+++ Sabur/revamp/src/page/contact/Contact.js
@@ -5,6 +5,9 @@ import './contact.css';
 import {Helmet} from "react-helmet";
 import favicon from "../../assets/images/title-icon.ico";
 import og_image from "../../assets/images/og-image.png";
+import {Link} from "react-router-dom";
+import ContactSection from "../../components/contact/ContactSection";
+import Testimonial from "../../components/testimonial/Testimonial";
 
 class Contact extends Component {
     render() {
@@ -21,7 +24,28 @@ class Contact extends Component {
                     <meta property="og:image" content={og_image} />
                 </Helmet>
 
-                <h2> Contact </h2>
+                {/* Hero Section */}
+                <div className="small-hero-bg lightDeepBG-2 pb-lg-0 pt-lg-0 pt-5 pb-3 d-flex align-items-center">
+                    <div className="container-fluid my-50">
+                        <div className="container">
+                            <div className="row">
+                                <div className="mx-auto col-12 col-md-8 col-md-6 text-center">
+                                    <p className="small-section-title mb-2 headline-2 breadcrumb-link"><Link
+                                        className="darkColor" to="/home"> Home </Link> / <Link className="darkColor" to="/mentor"> Contact </Link></p>
+                                    <h2 className="themeDarkColor fw-bold headline-1 hero-headline-1-res"> Have Questions? <br/> <span className="text-uppercase themeColor dot section-title headline-1-important-min-992">Contact us</span>
+                                    </h2>
+                                    <p className="themeDarkColor fs-5 my-4 opacity-80 mb-0"> Are you looking for
+                                        amazing e-course platform? <br className="d-none d-sm-block"/> Don't
+                                        worry! We got it for you! </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr className="bg-transparent p-0 m-0"/>
+                <ContactSection/>
+                {/* Google Map (API Key didn't get it) */}
+                <Testimonial/>
             </Fragment>
         );
     }
