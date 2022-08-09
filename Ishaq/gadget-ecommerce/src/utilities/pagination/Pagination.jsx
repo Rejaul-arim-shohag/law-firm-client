7ca0dd3140396e43d2161fe8068e78ea2ec144be
Md Ishaque <iamishaque.business@gmail.com>
2022-08-09 20:14:00 +0600

    Update brandlist

--- Ishaq/gadget-ecommerce/src/utilities/pagination/Pagination.jsx
+++ Ishaq/gadget-ecommerce/src/utilities/pagination/Pagination.jsx
@@ -10,7 +10,7 @@ export default function Pagination({ total, perPage,handlePageClick }) {
             marginPagesDisplayed={2}
             pageCount={Math.ceil(total / perPage)}
             previousLabel="<"
-            pageClassName="border py-1 px-3  duration-300 rounded-full hover:bg-primary text-white border-transparent bg-secondary "
+            pageClassName="border py-1 px-3 cursor-pointer duration-300 rounded-full hover:bg-primary text-white border-transparent bg-secondary "
             pageLinkClassName=" "
             previousClassName="border py-1 duration-300  px-3 rounded-full hover:bg-primary text-white border-transparent bg-secondary "
             previousLinkClassName=""
@@ -21,7 +21,7 @@ export default function Pagination({ total, perPage,handlePageClick }) {
             breakLinkClassName="page-link"
             containerClassName=" flex space-x-5"
             activeClassName="border py-1 px-3 rounded-full bg-primary text-white border-transparent"
-            renderOnZeroPageCount={null}
+            renderOnZeroPageCount={false}
         />
     )
 }
