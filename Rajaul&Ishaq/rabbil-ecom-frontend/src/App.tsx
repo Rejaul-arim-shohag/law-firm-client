4e3bad971a540424f8acb85830dcc4bae98537e4
Md Ishaque <iamishaque.business@gmail.com>
2022-08-21 15:35:04 +0600

    Update color palette

--- Rajaul&Ishaq/rabbil-ecom-frontend/src/App.tsx
+++ Rajaul&Ishaq/rabbil-ecom-frontend/src/App.tsx
@@ -1,14 +1,30 @@
-import { createTheme, ThemeProvider } from "@mui/material";
+import { colors, createTheme, ThemeProvider } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard.page";
 import Home from "./pages/Home.page";
 const theme = createTheme({
-  custom: {
-    primary: "#21BF73",
-    secondary: "#B0EACD",
-    light: "#F4F4F6",
-    dark: "#2E374F",
-    white: "#fff",
+  palette: {
+    custom: {
+      main: "#21BF73",
+      light: "#B0EACD",
+      dark: "#2E374F",
+      contrastText: "#F4F4F6",
+    }
+  },
+  typography: {
+    fontFamily: [
+      "Plus Jakarta Sans",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
   },
 });
 function App() {
--- Rajaul&Ishaq/rabbil-ecom-frontend/src/theme.d.ts
+++ Rajaul&Ishaq/rabbil-ecom-frontend/src/theme.d.ts
@@ -1,23 +1,12 @@
+import { PaletteColor, PaletteColorOptions } from "@mui/material"
 import { CSSProperties } from "react"
 
 declare module '@mui/material/styles' {
-    interface Theme {
-        custom: {
-            primary: string,
-            secondary: string,
-            light: string,
-            dark: string,
-            white: string
-        }
+    interface Palette {
+        custom?: PaletteColor
     }
-    interface ThemeOptions {
-        custom: {
-            primary: CSSProperties['color'],
-            secondary: CSSProperties['color'],
-            light: CSSProperties['color'],
-            dark: CSSProperties['color'],
-            white: CSSProperties['color']
-        }
+    interface PaletteOptions {
+        custom?: PaletteColorOptions
+    }
 
-    }
 }
